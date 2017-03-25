b039e23f110aa52bf1a1074e6d200a7167e444b4
David Caldwell <david@porkrind.org>
2017-03-25 12:45:55 -0700

    [renderjson.js] Use options parameter internally instead of a million arguments

--- renderjson.js
+++ renderjson.js
@@ -87,7 +87,7 @@ var module, window;
                                                    a.onclick = function(e) { callback(); if (e) e.stopPropagation(); return false; };
                                                    return a; };
 
-    function _renderjson(json, replacer, prop_list, indent, dont_indent, show_level, max_string, sort_objects) {
+    function _renderjson(json, indent, dont_indent, show_level, options) {
         var my_indent = dont_indent ? "" : indent;
 
         var disclosure = function(open, placeholder, close, type, builder) {
@@ -95,13 +95,13 @@ var module, window;
             var empty = span(type);
             var show = function() { if (!content) append(empty.parentNode,
                                                          content = prepend(builder(),
-                                                                           A(renderjson.hide, "disclosure",
+                                                                           A(options.hide, "disclosure",
                                                                              function() { content.style.display="none";
                                                                                           empty.style.display="inline"; } )));
                                     content.style.display="inline";
                                     empty.style.display="none"; };
             append(empty,
-                   A(renderjson.show, "disclosure", show),
+                   A(options.show, "disclosure", show),
                    themetext(type+ " syntax", open),
                    A(placeholder, null, show),
                    themetext(type+ " syntax", close));
@@ -115,8 +115,8 @@ var module, window;
         if (json === null) return themetext(null, my_indent, "keyword", "null");
         if (json === void 0) return themetext(null, my_indent, "keyword", "undefined");
 
-        if (typeof(json) == "string" && json.length > max_string)
-            return disclosure('"', json.substr(0,max_string)+" ...", '"', "string", function () {
+        if (typeof(json) == "string" && json.length > options.max_string_length)
+            return disclosure('"', json.substr(0,options.max_string_length)+" ...", '"', "string", function () {
                 return append(span("string"), themetext(null, my_indent, "string", JSON.stringify(json)));
             });
 
@@ -130,7 +130,7 @@ var module, window;
                 var as = append(span("array"), themetext("array syntax", "[", null, "\n"));
                 for (var i=0; i<json.length; i++)
                     append(as,
-                           _renderjson(replacer.call(json, i, json[i]), replacer, prop_list, indent+"    ", false, show_level-1, max_string, sort_objects),
+                           _renderjson(options.replacer.call(json, i, json[i]), indent+"    ", false, show_level-1, options),
                            i != json.length-1 ? themetext("syntax", ",") : [],
                            text("\n"));
                 append(as, themetext(null, indent, "array syntax", "]"));
@@ -139,20 +139,20 @@ var module, window;
         }
 
         // object
-        if (isempty(json, prop_list))
+        if (isempty(json, options.property_list))
             return themetext(null, my_indent, "object syntax", "{}");
 
         return disclosure("{", "...", "}", "object", function () {
             var os = append(span("object"), themetext("object syntax", "{", null, "\n"));
             for (var k in json) var last = k;
-            var keys = prop_list || Object.keys(json);
-            if (sort_objects)
+            var keys = options.property_list || Object.keys(json);
+            if (options.sort_objects)
                 keys = keys.sort();
             for (var i in keys) {
                 var k = keys[i];
                 if (!(k in json)) continue;
                 append(os, themetext(null, indent+"    ", "key", '"'+k+'"', "object syntax", ': '),
-                       _renderjson(replacer.call(json, k, json[k]), replacer, prop_list, indent+"    ", true, show_level-1, max_string, sort_objects),
+                       _renderjson(options.replacer.call(json, k, json[k]), indent+"    ", true, show_level-1, options),
                        k != last ? themetext("syntax", ",") : [],
                        text("\n"));
             }
@@ -163,31 +163,33 @@ var module, window;
 
     var renderjson = function renderjson(json)
     {
-        var replacer_f = typeof(renderjson.replacer) == "function" ? renderjson.replacer : function(k,v) { return v; };
-        var pre = append(document.createElement("pre"), _renderjson(json, replacer_f, renderjson.prop_list, "", false, renderjson.show_to_level, renderjson.max_string_length, renderjson.sort_objects));
+        var options = Object.assign({}, renderjson.options);
+        options.replacer = typeof(options.replacer) == "function" ? options.replacer : function(k,v) { return v; };
+        var pre = append(document.createElement("pre"), _renderjson(json, "", false, options.show_to_level, options));
         pre.className = "renderjson";
         return pre;
     }
-    renderjson.set_icons = function(show, hide) { renderjson.show = show;
-                                                  renderjson.hide = hide;
+    renderjson.set_icons = function(show, hide) { renderjson.options.show = show;
+                                                  renderjson.options.hide = hide;
                                                   return renderjson; };
-    renderjson.set_show_to_level = function(level) { renderjson.show_to_level = typeof level == "string" &&
+    renderjson.set_show_to_level = function(level) { renderjson.options.show_to_level = typeof level == "string" &&
                                                                                 level.toLowerCase() === "all" ? Number.MAX_VALUE
                                                                                                               : level;
                                                      return renderjson; };
-    renderjson.set_max_string_length = function(length) { renderjson.max_string_length = typeof length == "string" &&
+    renderjson.set_max_string_length = function(length) { renderjson.options.max_string_length = typeof length == "string" &&
                                                                                          length.toLowerCase() === "none" ? Number.MAX_VALUE
                                                                                                                          : length;
                                                           return renderjson; };
-    renderjson.set_sort_objects = function(sort_bool) { renderjson.sort_objects = sort_bool;
+    renderjson.set_sort_objects = function(sort_bool) { renderjson.options.sort_objects = sort_bool;
                                                         return renderjson; };
-    renderjson.set_replacer = function(replacer) { renderjson.replacer = replacer;
+    renderjson.set_replacer = function(replacer) { renderjson.options.replacer = replacer;
                                                    return renderjson; };
-    renderjson.set_property_list = function(prop_list) { renderjson.prop_list = prop_list;
+    renderjson.set_property_list = function(prop_list) { renderjson.options.property_list = prop_list;
                                                          return renderjson; };
     // Backwards compatiblity. Use set_show_to_level() for new code.
-    renderjson.set_show_by_default = function(show) { renderjson.show_to_level = show ? Number.MAX_VALUE : 0;
+    renderjson.set_show_by_default = function(show) { renderjson.options.show_to_level = show ? Number.MAX_VALUE : 0;
                                                       return renderjson; };
+    renderjson.options = {};
     renderjson.set_icons('⊕', '⊖');
     renderjson.set_show_by_default(false);
     renderjson.set_sort_objects(false);
